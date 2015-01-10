d15700f43ce667b3563d16456dac344b4fb91bd2
ardoric <ardoric@gmail.com>
2015-01-10 16:40:46 +0000

    added install instructions
    installer script for java
    install.txt for .net

---
+++ J2EE/install_ardopgsql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /etc/sysconfig/outsystems
+
+umask 022
+
+# this is for jboss 7 / jboss 6 eap.
+
+mkdir -p $JBOSS_HOME/modules/outsystems/ardopgsql/
+cp postgresql-9.3-1102.jdbc3.jar module.xml $JBOSS_HOME/modules/outsystems/ardopgsql/
+cp postgresqldatabaseprovider.jar $OUTSYSTEMS_HOME/plugins/database
+
+chown -R jboss:jboss $JBOSS_HOME/modules/outsystems/ardopgsql/
+chown outsystems:outsystems $OUTSYSTEMS_HOME/plugins/database/postgresqldatabaseprovider.jar
+
+$JBOSS_HOME/bin/jboss-cli.sh -c --command='/subsystem=ee:write-attribute(name=global-modules, value=[{"name" => "outsystems", "slot" => "main"},{"name" => "outsystems", "slot" => "ardopgsql"}])'
+
+$OUTSYSTEMS_HOME/configurationtool.sh /upgradeinstall /silent /scinstall
