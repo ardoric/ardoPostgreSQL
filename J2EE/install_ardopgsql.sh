0e2063e7268e0e0969ade302d2fce2078296d615
ardoric <ardoric@gmail.com>
2015-01-24 14:27:55 +0000

    added postgresql libs to lib
    This fixes issue with not being able to test query / preview aggregate.

--- J2EE/install_ardopgsql.sh
+++ J2EE/install_ardopgsql.sh
@@ -9,9 +9,11 @@ umask 022
 mkdir -p $JBOSS_HOME/modules/outsystems/ardopgsql/
 cp postgresql-9.3-1102.jdbc3.jar module.xml $JBOSS_HOME/modules/outsystems/ardopgsql/
 cp postgresqldatabaseprovider.jar $OUTSYSTEMS_HOME/plugins/database
+cp postgresql-9.3-1102.jdbc3.jar $OUTSYSTEMS_HOME/lib
 
 chown -R jboss:jboss $JBOSS_HOME/modules/outsystems/ardopgsql/
 chown outsystems:outsystems $OUTSYSTEMS_HOME/plugins/database/postgresqldatabaseprovider.jar
+chown outsystems:outsystems $OUTSYSTEMS_HOME/lib/postgresql-9.3-1102.jdbc3.jar
 
 $JBOSS_HOME/bin/jboss-cli.sh -c --command='/subsystem=ee:write-attribute(name=global-modules, value=[{"name" => "outsystems", "slot" => "main"},{"name" => "outsystems", "slot" => "ardopgsql"}])'
 
