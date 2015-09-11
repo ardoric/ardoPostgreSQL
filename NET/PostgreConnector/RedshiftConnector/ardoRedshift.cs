367cb79eefe7dadb19a3ab62f74f40bfc18dd7a1
ardoric <ardoric@gmail.com>
2015-09-11 18:30:03 +0100

    begin support for redshift
    - added redshift database connector for .NET
    - upgraded npgsql version (to 2.2.5, first one to support redshift, last
    one to have binaries available)

---
+++ NET/PostgreConnector/RedshiftConnector/ardoRedshift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ardo.DatabaseProvider.PostgreSQL;
+using OutSystems.RuntimeCommon;
+
+namespace ardo.DatabaseProvider.Redshift
+{
+    public class ardoRedshift : ardoPGSQL
+    {
+        public override DatabaseProviderKey Key
+        {
+            get { return DatabaseProviderKey.Deserialize("ardoRedshift"); }
+        }
+
+    }
+}
