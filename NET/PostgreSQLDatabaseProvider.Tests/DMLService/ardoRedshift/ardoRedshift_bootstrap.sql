e5aa3306775b46ab5068708fdc287434f5b33b2e
ardoric <ardoric@gmail.com>
2016-11-16 02:15:04 +0000

    continue of previous commit

--- NET/PostgreSQLDatabaseProvider.Tests/DMLService/ardoRedshift/ardoRedshift_bootstrap.sql
+++ NET/PostgreSQLDatabaseProvider.Tests/DMLService/ardoRedshift/ardoRedshift_bootstrap.sql
@@ -1,5 +1,6 @@
 --- cheating with DUMMY because DUMMY and "DUMMY" are not the same in PGSQL
 -- CREATE TABLE "DUMMY%Machine%" (ID serial not null primary key, VAL int, "SELECT" int);
+CREATE TABLE DUMMY%Machine% (ID int IDENTITY(1,1) not null primary key, VAL int, "SELECT" int);
 CREATE TABLE PERSON%Machine% (PERSONID int, NAME varchar(255), AGE int, WEIGHT decimal(37, 8), DEAD boolean, LASTLOGIN timestamp);
 -- INSERT INTO "DUMMY%Machine%" (VAL, "SELECT") VALUES (2,123) ;
 INSERT INTO DUMMY%Machine% (VAL, "SELECT") VALUES (2,123) ;
