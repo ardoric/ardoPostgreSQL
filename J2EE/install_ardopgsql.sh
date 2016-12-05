829673bf3087806d0f99d68f9464042a587e27eb
ardoric <ardoric@gmail.com>
2016-12-05 00:21:32 +0000

    updated java installer

--- J2EE/install_ardopgsql.sh
+++ J2EE/install_ardopgsql.sh
@@ -4,20 +4,36 @@
 
 source /etc/sysconfig/outsystems
 
+if [ -n "$WILDFLY_HOME" ]; then
+	APP_SERVER_HOME=$WILDFLY_HOME
+	USER=wildfly
+fi
+
+if [ -n "$JBOSS_HOME" ]; then
+	APP_SERVER_HOME=$JBOSS_HOME
+	USER=jboss
+fi
+
+if [ -z $APP_SERVER_HOME ]; then
+	echo Unsupported app server. Only Wildfly and JBoss are supported for automatic installation.
+	echo If you are using this in Weblogic, please get in contact with the author.
+	exit
+fi
+
 umask 022
 
-# this is for jboss 7 / jboss 6 eap.
+# this is for Wildfly or JBoss EAP 6.
 
-mkdir -p $JBOSS_HOME/modules/outsystems/ardopgsql/
-cp postgresql-9.4-1201.jdbc4.jar module.xml $JBOSS_HOME/modules/outsystems/ardopgsql/
+mkdir -p $APP_SERVER_HOME/modules/outsystems/ardopgsql/
+cp postgresql-9.4.1212.jar module.xml $APP_SERVER_HOME/modules/outsystems/ardopgsql/
 cp ardo.databaseprovider.postgresql.jar $OUTSYSTEMS_HOME/plugins/database
-cp postgresql-9.4-1201.jdbc4.jar $OUTSYSTEMS_HOME/lib
+cp postgresql-9.4.1212.jar $OUTSYSTEMS_HOME/lib
 
-chown -R jboss:jboss $JBOSS_HOME/modules/outsystems/ardopgsql/
+chown -R wildfly:wildfly $APP_SERVER_HOME/modules/outsystems/ardopgsql/
 chown outsystems:outsystems $OUTSYSTEMS_HOME/plugins/database/ardo.databaseprovider.postgresql.jar
-chown outsystems:outsystems $OUTSYSTEMS_HOME/lib/postgresql-9.4-1201.jdbc4.jar
+chown outsystems:outsystems $OUTSYSTEMS_HOME/lib/postgresql-9.4.1212.jar
 
-$JBOSS_HOME/bin/jboss-cli.sh -c --command='/subsystem=ee:write-attribute(name=global-modules, value=[{"name" => "outsystems", "slot" => "main"},{"name" => "outsystems", "slot" => "ardopgsql"}])'
+$APP_SERVER_HOME/bin/jboss-cli.sh --user=osdeployservice --password=outsystems -c --command='/subsystem=ee:write-attribute(name=global-modules, value=[{"name" => "outsystems", "slot" => "main"},{"name" => "outsystems", "slot" => "ardopgsql"}])'
 
 source /etc/outsystems/os.services.conf
 
